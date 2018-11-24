ec5c53f7fb20d7bbd43a626b22e4d618655ceaec
Cristóvão D. Sousa <crisjss@gmail.com>
2018-11-24 00:58:12 +0000

    Use LLVM 6.0.0

--- deps/llvm-patches/apply-llvm-patches.mk
+++ deps/llvm-patches/apply-llvm-patches.mk
@@ -8,43 +8,46 @@ $$(LLVM_SRC_DIR)/$1.patch-applied: $$(LLVM_SRC_DIR) | llvm-patches/$1.patch $$(L
 LLVM_PATCH_PREV := $$(LLVM_SRC_DIR)/$1.patch-applied
 LLVM_PATCH_LIST += $$(LLVM_PATCH_PREV)
 endef
-ifeq ($(LLVM_VER),3.3)
-$(eval $(call LLVM_PATCH,llvm-3.3))
-$(eval $(call LLVM_PATCH,instcombine-llvm-3.3))
-$(eval $(call LLVM_PATCH,int128-vector.llvm-3.3))
-$(eval $(call LLVM_PATCH,osx-10.10.llvm-3.3))
-$(eval $(call LLVM_PATCH,win64-int128.llvm-3.3))
-else ifeq ($(LLVM_VER_SHORT),3.7)
-ifeq ($(LLVM_VER),3.7.0)
-$(eval $(call LLVM_PATCH,llvm-3.7.0))
+
+ifeq ($(LLVM_VER_SHORT),6.0)
+ifeq ($(LLVM_VER_PATCH), 0)
+$(eval $(call LLVM_PATCH,llvm-D27629-AArch64-large_model_4.0))
+else
+$(eval $(call LLVM_PATCH,llvm-D27629-AArch64-large_model_6.0.1))
 endif
-$(eval $(call LLVM_PATCH,llvm-3.7.1))
-$(eval $(call LLVM_PATCH,llvm-3.7.1_2))
-$(eval $(call LLVM_PATCH,llvm-3.7.1_3))
-$(eval $(call LLVM_PATCH,llvm-3.7.1_symlinks))
-$(eval $(call LLVM_PATCH,llvm-3.8.0_bindir))
-$(eval $(call LLVM_PATCH,llvm-D14260))
-$(eval $(call LLVM_PATCH,llvm-nodllalias))
-$(eval $(call LLVM_PATCH,llvm-D21271-instcombine-tbaa-3.7))
-$(eval $(call LLVM_PATCH,llvm-win64-reloc-dwarf))
-else ifeq ($(LLVM_VER_SHORT),3.8)
-ifeq ($(LLVM_VER),3.8.0)
-$(eval $(call LLVM_PATCH,llvm-D17326_unpack_load))
+$(eval $(call LLVM_PATCH,llvm-D34078-vectorize-fdiv))
+$(eval $(call LLVM_PATCH,llvm-6.0-NVPTX-addrspaces)) # NVPTX
+$(eval $(call LLVM_PATCH,llvm-D42262-jumpthreading-not-i1)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-PPC-addrspaces)) # remove for 7.0
+ifeq ($(LLVM_VER_PATCH), 0)
+$(eval $(call LLVM_PATCH,llvm-D42260)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-rL326843-missing-header)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-6.0-r327540)) # remove for 7.0
 endif
-$(eval $(call LLVM_PATCH,llvm-3.7.1_3)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-D14260))
-$(eval $(call LLVM_PATCH,llvm-3.8.0_bindir)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-3.8.0_winshlib)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-nodllalias)) # Remove for 3.9
-# Cygwin and openSUSE still use win32-threads mingw, https://llvm.org/bugs/show_bug.cgi?id=26365
-$(eval $(call LLVM_PATCH,llvm-3.8.0_threads))
-# fix replutil test on unix
-$(eval $(call LLVM_PATCH,llvm-D17165-D18583)) # Remove for 3.9
-# Segfault for aggregate load
-$(eval $(call LLVM_PATCH,llvm-D17712)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-PR26180)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-PR27046)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-3.8.0_ppc64_SUBFC8)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-D21271-instcombine-tbaa-3.8)) # Remove for 3.9
-$(eval $(call LLVM_PATCH,llvm-win64-reloc-dwarf))
+$(eval $(call LLVM_PATCH,llvm-6.0.0_D27296-libssp)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-6.0-D44650)) # mingw32 build fix
+ifeq ($(LLVM_VER_PATCH), 0)
+$(eval $(call LLVM_PATCH,llvm-D45008)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-D45070)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-6.0.0-ifconv-D45819)) # remove for 7.0
+endif
+$(eval $(call LLVM_PATCH,llvm-D46460))
+ifeq ($(LLVM_VER_PATCH), 0)
+$(eval $(call LLVM_PATCH,llvm-rL332680)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-rL332682)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-rL332302)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-rL332694)) # remove for 7.0
+endif
+$(eval $(call LLVM_PATCH,llvm-rL327898)) # remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-6.0-DISABLE_ABI_CHECKS))
+$(eval $(call LLVM_PATCH,llvm-OProfile-line-num))
+$(eval $(call LLVM_PATCH,llvm-D44892-Perf-integration))
+$(eval $(call LLVM_PATCH,llvm-D49832-SCEVPred)) # Remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-rL323946-LSRTy)) # Remove for 7.0
+$(eval $(call LLVM_PATCH,llvm-D50010-VNCoercion-ni))
+$(eval $(call LLVM_PATCH,llvm-D50167-scev-umin))
+$(eval $(call LLVM_PATCH,llvm-rL326967-aligned-load)) # remove for 7.0
+ifeq ($(LLVM_VER_PATCH), 0)
+$(eval $(call LLVM_PATCH,llvm-windows-race))
+endif
 endif # LLVM_VER
