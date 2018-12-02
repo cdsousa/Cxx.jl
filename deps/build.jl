50e735624ecd2436bd66ec9f6f26e2d5313beb59
Cristóvão D. Sousa <crisjss@gmail.com>
2018-12-02 10:35:20 +0000

    Make progresses with BinaryProvider and BinDeps

--- deps/build.jl
+++ deps/build.jl
@@ -1,32 +1,62 @@
 
-include("build_LLVM.v6.0.0.jl")
+LLVM_VER = "6.0.0"
 
 
-mkpath("build")
-mkpath("usr/lib")
+
+BASE_JULIA_BIN = get(ENV, "BASE_JULIA_BIN", Sys.BINDIR)
+BASE_JULIA_SRC = get(ENV, "BASE_JULIA_SRC", joinpath(BASE_JULIA_BIN, "..", ".."))
+println("writing path.jl file")
+s = """
+    const BASE_JULIA_BIN=$(sprint(show, BASE_JULIA_BIN))
+    export BASE_JULIA_BIN
+    const BASE_JULIA_SRC=$(sprint(show, BASE_JULIA_SRC))
+    export BASE_JULIA_SRC
+    """
+f = open(joinpath(dirname(@__FILE__),"path.jl"), "w")
+write(f, s)
+close(f)
+
+
+
+module LLVMBuilder
+    import ..LLVM_VER
+    include("build_LLVM.v$LLVM_VER.jl")
+end
+
+
+using BinDeps
+
+@BinDeps.setup
+
+
+cxxffi = library_dependency("libcxxffi")
+
+provides(Sources, URI("http://releases.llvm.org/$LLVM_VER/cfe-$LLVM_VER.src.tar.xz"), cxxffi)
 
 
 CXX="g++"
 
+CLANG_SRC_DIR = "$(BinDeps.srcdir(cxxffi))/cfe-$LLVM_VER.src/lib/"
 
 CXX_ABI_SETTING="-D_GLIBCXX_USE_CXX11_ABI=1"
-INCLUDE_DIRECTORIES = ["-I$(Sys.BINDIR)/../include", "-Iusr/include", "-IclangCodeGenHeaders"]
+INCLUDE_DIRECTORIES = ["-I$(Sys.BINDIR)/../include", "-Iusr/include", "-I$(CLANG_SRC_DIR)"]
 LLVM_EXTRA_CPPFLAGS = "-DLLVM_NDEBUG"
-
-cmd = `$CXX $(CXX_ABI_SETTING) -fno-rtti -DLIBRARY_EXPORTS -fPIC -O0 -g -std=c++11 $INCLUDE_DIRECTORIES $LLVM_EXTRA_CPPFLAGS -c ../src/bootstrap.cpp -o build/bootstrap.o`
-
-run(cmd)
-
 
 LINK_DIRECTORIES = ["-L$(Sys.BINDIR)/../lib", "-L$(Sys.BINDIR)/../lib/julia", "-Lusr/lib"]
 
 LINK_LIBS = ["-lclangFrontendTool", "-lclangBasic", "-lclangLex", "-lclangDriver", "-lclangFrontend", "-lclangParse", "-lclangAST", "-lclangASTMatchers", "-lclangSema", "-lclangAnalysis", "-lclangEdit", "-lclangRewriteFrontend", "-lclangRewrite", "-lclangSerialization", "-lclangStaticAnalyzerCheckers", "-lclangStaticAnalyzerCore", "-lclangStaticAnalyzerFrontend", "-lclangTooling", "-lclangToolingCore", "-lclangCodeGen", "-lclangARCMigrate", "-lclangFormat"]
 
-cmd = `$CXX -shared -fPIC $LINK_DIRECTORIES -ljulia -lLLVM -o usr/lib/libcxxffi.so -Wl,--whole-archive $LINK_LIBS -Wl,--no-whole-archive build/bootstrap.o`
 
-run(cmd)
+provides(BuildProcess,
+    (@build_steps begin
+        GetSources(cxxffi)
+        CreateDirectory("build")
+        @build_steps begin
+             `$CXX $(CXX_ABI_SETTING) -fno-rtti -DLIBRARY_EXPORTS -fPIC -O0 -g -std=c++11 $INCLUDE_DIRECTORIES $LLVM_EXTRA_CPPFLAGS -c ../src/bootstrap.cpp -o build/bootstrap.o`
+             `$CXX -shared -fPIC $LINK_DIRECTORIES -ljulia -lLLVM -o usr/lib/libcxxffi.so -Wl,--whole-archive $LINK_LIBS -Wl,--no-whole-archive build/bootstrap.o`
+             `$CXX -E -P $INCLUDE_DIRECTORIES -DJULIA ../src/cenumvals.jl.h -o build/clang_constants.jl`
+        end
+    end), cxxffi)
 
 
-cmd = `$CXX -E -P $INCLUDE_DIRECTORIES -DJULIA ../src/cenumvals.jl.h`
-cenumvals = readstring(cmd)
-write("build/clang_constants.jl", cenumvals)
+@BinDeps.install Dict(:cxxffi => :cxxffi)
